d879759066e07e28bcbd397574d5ef609ff76663
evanwhit225 <evanwhit225@SDS-L-H85HC63>
2024-02-09 14:12:34 -0500

    Add project files.

---
+++ StartGame 2/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace StartGame_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StartLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(338, 110);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(157, 101);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StartLabel
+            // 
+            this.StartLabel.AutoSize = true;
+            this.StartLabel.ForeColor = System.Drawing.Color.Black;
+            this.StartLabel.Location = new System.Drawing.Point(360, 272);
+            this.StartLabel.Name = "StartLabel";
+            this.StartLabel.Size = new System.Drawing.Size(110, 13);
+            this.StartLabel.TabIndex = 1;
+            this.StartLabel.Text = "Game Will Start In 3...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StartLabel);
+            this.Controls.Add(this.StartButton);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ready Go";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label StartLabel;
+    }
+}
+
---
+++ StartGame 2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace StartGame_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click (object sender, EventArgs e)
+        {
+            StartButton.Visible = false;
+            StartLabel.ForeColor = Color.White;
+            SoundPlayer alertPlayer = new SoundPlayer(Properties.Resources.alertSound);
+            alertPlayer.Play();
+            
+            Refresh();
+            Thread.Sleep(1000);
+
+            StartLabel.Text = "Game Will Start In 2...";
+           
+            Refresh();
+            Thread.Sleep(1000);
+
+            StartLabel.Text = "Game Will Start In 1...";
+            Refresh();
+
+            Thread.Sleep(1000);
+            alertPlayer.Stop();
+            SoundPlayer alertSound2 = new SoundPlayer(Properties.Resources.alertSound2);
+            alertSound2.Play();
+            StartLabel.Text = "GOOO!!";
+            StartLabel.BackColor = Color.Green;
+            this.BackColor = Color.Green;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
